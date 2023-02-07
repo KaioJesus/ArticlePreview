0ad3b786156766b43822ae29d39d6b5d6bef0dec
Kaio Jesus <kaioramos990@hotmail.com>
2023-02-07 00:39:47 -0300

    style mobile finshed, pc just need to fix button activated

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const perfil = document.querySelector('.card__div-container');
+const midias = document.querySelector('.social__medias');
+
+function onSubmit() {
+    perfil.style.display = 'none';
+    midias.style.display = 'block';
+  };
